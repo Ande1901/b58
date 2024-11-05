aad58851c1d6c81c94323e539c283249369b5db7
Ande1901 <andefranata@gmail.com>
2024-11-05 21:51:09 +0700

    bebas

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 1000;
+const path = require("path");
+
+app.set("view engine", "hbs");
+
+app.use("/assets/image", express.static("assets/image"));
+app.use("/assets/css", express.static("assets/css"));
+app.use("/assets/js", express.static("assets/js"));
+app.set("views", path.join(__dirname, "assets/views"));
 
 // routing
-app.get("/", (req, res) => {
-  res.send("Hello Worldd!");
-});
+app.get("/", home);
+app.get("/addproject", addProject);
+app.get("/contact", contact);
+app.get("/testimonial", testimonial);
+app.get("/project/:id", project);
+
+function home(req, res) {
+  res.render("index");
+}
+
+function addProject(req, res) {
+  res.render("addproject");
+}
+
+function contact(req, res) {
+  res.render("contact");
+}
+
+function testimonial(req, res) {
+  res.render("testimonial");
+}
+
+function project(req, res) {
+  const id = req.params.id;
+
+  res.render("project");
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
